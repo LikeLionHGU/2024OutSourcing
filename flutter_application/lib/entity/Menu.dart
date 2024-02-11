a83d5bb893cc243d723be2a9cf769f6b2d8d2b0d
김동규 <kdg1941@naver.com>
2024-02-11 23:52:37 +0900

    feat: 메인 페이지 TabBar List 개별 렌더링 기능 구현

--- flutter_application/lib/entity/Menu.dart
+++ flutter_application/lib/entity/Menu.dart
@@ -4,9 +4,11 @@ class Menu {
   final int count;
   final int price;
   final String description;
+  final int category;
 
   Menu({
     required this.name,
+    required this.category,
     required this.imageAddress,
     required this.count,
     required this.price,
@@ -15,7 +17,9 @@ class Menu {
 
   // Firestore 문서의 데이터를 받아서 Menu 객체를 생성하는 메서드
   factory Menu.fromFirestore(Map<String, dynamic> firestoreData) {
+
     return Menu(
+      category: firestoreData['categoryIndex'] is int ? firestoreData['categoryIndex'] : int.tryParse(firestoreData['categoryIndex'].toString()) ?? 0,
       name: firestoreData['name'] ?? '', // 필드가 존재하지 않을 경우를 대비한 기본값 설정
       imageAddress: firestoreData['imageUrl'] ?? '',
       count: firestoreData['count'] is int ? firestoreData['count'] : int.tryParse(firestoreData['count'].toString()) ?? 0,
@@ -26,3 +30,4 @@ class Menu {
 }
 
 
+// 콩자반
